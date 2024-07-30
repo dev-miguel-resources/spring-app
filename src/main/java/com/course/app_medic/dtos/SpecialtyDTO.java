08e33af844e849a204f88c7ccb2d3d1c22305a41
dev-miguel-resources <ingenieromiguelch@gmail.com>
2024-07-30 11:13:37 -0400

    working in new controllers with cruds

---
+++ src/main/java/com/course/app_medic/dtos/SpecialtyDTO.java
@@ -0,0 +1,29 @@
+package com.course.app_medic.dtos;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class SpecialtyDTO {
+
+    @EqualsAndHashCode.Include
+    private Integer idSpecialty;
+
+    @NotEmpty
+    @NotNull
+    private String name;
+
+    @NotEmpty
+    @NotNull
+    private String description;
+
+}
